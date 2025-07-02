54775f2e3d0df75ea7d7ae2fdd8c3ef4a61613f2
Simar <simar.narula14012005@gmail.com>
2025-07-02 16:45:06 +0530

    cos

--- plot_curve.py
+++ plot_curve.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 
 theta = np.deg2rad(np.arange(0, 360, 1))
 
-A = 5
+A = 200
 B = 10
 phi = np.deg2rad(30)
 
-sin = A*np.sin(theta - phi) + B
+sin = A*np.cos(theta - phi) + B
 
 plt.plot(theta, sin)
 plt.xlabel("$\\theta$")
