985bf58b544a9cab261f00efa609ada1e1f2c2f1
Sujay Mate <sujay.mate@gmail.com>
2025-07-02 16:41:21 +0530

    Add demo code for merge conflict

---
+++ plot_curve.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+theta = np.deg2rad(np.arange(0, 360, 1))
+
+A = 5
+B = 10
+phi = np.deg2rad(30)
+
+sin = A*np.sin(theta - phi) + B
+
+plt.plot(theta, sin)
+plt.xlabel("$\\theta$")
+plt.ylabel("Amplitude")
+plt.show()
